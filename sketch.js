d214c76ec58faa893815aa8425c3407b8f8f6109
dvb7 <73330595+dvb7@users.noreply.github.com>
2020-11-06 09:15:09 +0100

    Added Emotion

--- sketch.js
+++ sketch.js
@@ -4,28 +4,51 @@ function setup() {
 } 
 
 function draw() { 
-	background(131, 175, 155);
+	background('rgb(131,175,155)'); //Jade
    
-   translate(300, 100);
+   drawHappyFace(300, 100, false);
+}
 
+function drawHappyFace(x, y, emo){
+   
    //Face
-   fill(249,205,173);//rosy beige
-   ellipse(0, 0, 100, 100);
+   if (emo) {
+      fill('rgb(135,206,250)');//Healthy Blue
+   } 
+   else {
+      fill('rgb(60,179,113)');//Sick Green
+   }
+   ellipse(x, y, 100, 100);
 
-   //Eye 1
-   fill(30);//dark gray
-   ellipse(0, 10, 10, 10);
+   //Eyes
+   if (emo) {
+      //Wonky Eye 1
+      fill(30);//dark gray
+      ellipse(x-25, y-10, 10, 10);
 
-   //Eye 2
-   ellipse(20, 10, 10, 10);
+      //Wonky Eye 2
+      ellipse(x+25, y-20, 10, 10);
+   } 
+   else {
+      //Sick Eye 1
+      stroke(30);//dark gray
+      strokeWeight(10);//Thicc
+      line(x-25, y-10, x-15, y-20)
+
+      //Sick Eye 2
+      line(x+15, y-20, x+25, y-10)
+
+      noStroke();
+   }
 
    //Mouth
-   fill(252,157,154);//light pink
-   arc(0, 25, 30, 30, 0, radians(180), PIE); 
-
-}
-
-function drawHappyFace(){
-	//Her kommer i selv til at skrive kode
-}
+   if (emo) {
+      fill(252,157,154);//light pink
+      arc(x, y+25, 30, 30, 0, radians(180), PIE); 
+   }
+   else {
+      fill(252,157,154);//light pink
+      arc(x, y+25, 30, 30, radians(180), 0, PIE); 
+   }
+};
 
